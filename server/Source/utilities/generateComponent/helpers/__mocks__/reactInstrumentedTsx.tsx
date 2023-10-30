fa2e92cfe233cc4c045fea452be504710b0e19a3
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-30 12:18:40 +0200

    squash!

--- server/Source/utilities/generateComponent/helpers/__mocks__/reactInstrumentedTsx.tsx
+++ server/Source/utilities/generateComponent/helpers/__mocks__/reactInstrumentedTsx.tsx
@@ -1,8 +1,6 @@
 // This file is being ignored by typescript because it is only used in tests.
 
 // @ts-ignore
-class ReactInstrumented extends Component {
-
-}
+class ReactInstrumented extends Component {}
 // @ts-ignore
 export default withMezzurite(ReactInstrumented);
