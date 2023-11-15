e1fd89bacd46cf4e6dc99983457c38bd1c4b976a
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-15 20:11:52 +0200

    squash!

--- server/src/utilities/generateComponent/helpers/index.ts
+++
@@ -1,9 +0,0 @@
-import generateNgComponent from './generateNgComponent';
-import generateNgModule from './generateNgModule';
-import generateReact from './generateReact';
-
-export default {
-  generateNgComponent,
-  generateNgModule,
-  generateReact
-};
