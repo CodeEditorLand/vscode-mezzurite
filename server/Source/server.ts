b7f06f4998e9aae2cf7f38724ef5d93d5386ebdc
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-17 16:33:44 +0200

    squash!

--- server/Source/server.ts
+++ server/Source/server.ts
@@ -3,19 +3,19 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import { join } from 'path';
-import Project from 'ts-morph';
+import { join } from "path";
+import Project from "ts-morph";
 import {
-  createConnection,
-  TextDocuments,
-  ProposedFeatures,
-  WorkspaceFolder
-} from 'vscode-languageserver';
+	createConnection,
+	TextDocuments,
+	ProposedFeatures,
+	WorkspaceFolder,
+} from "vscode-languageserver";
 
-import onFileChanged from './events/onFileChanged';
-import MezzuriteComponent from './models/MezzuriteComponent';
-import combineWorkspaceFolders from './utilities/combineWorkspaceFolders';
-import processFile from './utilities/processFile';
+import onFileChanged from "./events/onFileChanged";
+import MezzuriteComponent from "./models/MezzuriteComponent";
+import combineWorkspaceFolders from "./utilities/combineWorkspaceFolders";
+import processFile from "./utilities/processFile";
 
 // Create a connection for the server. The connection uses Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
@@ -27,55 +27,70 @@ const documents: TextDocuments = new TextDocuments();
 
 let components: MezzuriteComponent[] = [];
 const project = new Project({
-  addFilesFromTsConfig: false
+	addFilesFromTsConfig: false,
 });
 
 connection.onInitialize(() => {
-  return {
-    capabilities: {
-      textDocumentSync: documents.syncKind,
+	return {
+		capabilities: {
+			textDocumentSync: documents.syncKind,
 			// Tell the client that the server supports code completion
-      completionProvider: {
-        resolveProvider: true
-      }
-    }
-  };
+			completionProvider: {
+				resolveProvider: true,
+			},
+		},
+	};
 });
 
 connection.onInitialized(() => {
-  connection.workspace.getWorkspaceFolders().then((folders: WorkspaceFolder[]) => {
-    const files = combineWorkspaceFolders(folders);
-    Promise.all(
-      files.map((filePath: string) => {
-        return processFile(filePath, project)
-          .catch((error: Error) => {
-            console.warn(`Unable to process ${filePath}: ${error}`);
-            return null;
-          });
-      })
-    )
-    .then((results: MezzuriteComponent[]) => {
-      components = results.filter((component: MezzuriteComponent) => component != null);
-      connection.sendNotification('custom/mezzuriteComponents', { value: components });
-    })
-    .catch((error: Error) => connection.console.warn(error.message));
-  });
+	connection.workspace
+		.getWorkspaceFolders()
+		.then((folders: WorkspaceFolder[]) => {
+			const files = combineWorkspaceFolders(folders);
+			Promise.all(
+				files.map((filePath: string) => {
+					return processFile(filePath, project).catch(
+						(error: Error) => {
+							console.warn(
+								`Unable to process ${filePath}: ${error}`
+							);
+							return null;
+						}
+					);
+				})
+			)
+				.then((results: MezzuriteComponent[]) => {
+					components = results.filter(
+						(component: MezzuriteComponent) => component != null
+					);
+					connection.sendNotification("custom/mezzuriteComponents", {
+						value: components,
+					});
+				})
+				.catch((error: Error) =>
+					connection.console.warn(error.message)
+				);
+		});
 });
 
-connection.onNotification('custom/fileChanged', (filePath: string) => {
-  onFileChanged(components, filePath, project)
-    .then((updatedComponents: MezzuriteComponent[]) => {
-      components = updatedComponents;
-      connection.sendNotification('custom/mezzuriteComponents', { value: components });
-    })
-    .catch((error: Error) => console.warn(error.message));
+connection.onNotification("custom/fileChanged", (filePath: string) => {
+	onFileChanged(components, filePath, project)
+		.then((updatedComponents: MezzuriteComponent[]) => {
+			components = updatedComponents;
+			connection.sendNotification("custom/mezzuriteComponents", {
+				value: components,
+			});
+		})
+		.catch((error: Error) => console.warn(error.message));
 });
 
-connection.onNotification('custom/fileDeleted', (filePath: string) => {
-  components = components.filter((component: MezzuriteComponent) => {
-    return join(component.filePath) !== join(filePath);
-  });
-  connection.sendNotification('custom/mezzuriteComponents', { value: components });
+connection.onNotification("custom/fileDeleted", (filePath: string) => {
+	components = components.filter((component: MezzuriteComponent) => {
+		return join(component.filePath) !== join(filePath);
+	});
+	connection.sendNotification("custom/mezzuriteComponents", {
+		value: components,
+	});
 });
 
 documents.listen(connection);
--- server/Source/utilities/combineWorkspaceFolders.ts
+++ server/Source/utilities/combineWorkspaceFolders.ts
@@ -1,22 +1,23 @@
-import { WorkspaceFolder } from 'vscode-languageserver';
-import extractSourceFiles from './extractSourceFiles';
+import { WorkspaceFolder } from "vscode-languageserver";
+import extractSourceFiles from "./extractSourceFiles";
 
-function combineWorkspaceFolders (folders: WorkspaceFolder[]): string[] {
-  let filePaths: string[] = [];
+function combineWorkspaceFolders(folders: WorkspaceFolder[]): string[] {
+	let filePaths: string[] = [];
 
-  if (folders != null && folders.length > 0) {
-    folders.forEach((folder: WorkspaceFolder): void => {
-      if (folder != null) {
-        const folderFilePaths = extractSourceFiles(folder.uri)
-          .filter((file: string): boolean => {
-            return file != null;
-          });
-        filePaths = [ ...filePaths, ...folderFilePaths ];
-      }
-    });
-  }
+	if (folders != null && folders.length > 0) {
+		folders.forEach((folder: WorkspaceFolder): void => {
+			if (folder != null) {
+				const folderFilePaths = extractSourceFiles(folder.uri).filter(
+					(file: string): boolean => {
+						return file != null;
+					}
+				);
+				filePaths = [...filePaths, ...folderFilePaths];
+			}
+		});
+	}
 
-  return filePaths;
+	return filePaths;
 }
 
 export default combineWorkspaceFolders;
