d983013ad6125cc43a14bbfe62a61d76d1a54f55
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-20 18:08:21 +0200

    squash!

--- server/Source/utilities/generateComponent/helpers/__mocks__/ngModuleInstrumented.ts
+++ server/Source/utilities/generateComponent/helpers/__mocks__/ngModuleInstrumented.ts
@@ -1,19 +1,20 @@
 // This file is being ignored by typescript because it is only used in tests.
 
 // @ts-ignore
-import { AngularPerfModule, RoutingService } from '@microsoft/mezzurite-angular';
+import {
+	AngularPerfModule,
+	RoutingService,
+} from "@microsoft/mezzurite-angular";
 
 // @ts-ignore
 @NgModule({
-  imports: [
-    AngularPerfModule.forRoot()
-  ]
+	imports: [AngularPerfModule.forRoot()],
 })
 // @ts-ignore
 export class InstrumentedModule {
-  // @ts-ignore
-  constructor (@Inject(RoutingService) private router: typeof RoutingService) {
-    // @ts-ignore
-    router.start();
-  }
+	// @ts-ignore
+	constructor(@Inject(RoutingService) private router: typeof RoutingService) {
+		// @ts-ignore
+		router.start();
+	}
 }
